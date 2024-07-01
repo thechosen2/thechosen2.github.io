3c9b8182b8aaf99d9ebfa2be8dee56dcfc93dc8f
thechosen2 <pravmalpani@gmail.com>
2024-07-02 03:21:40 +0530

    Camera css fixed

--- src/pages/camera.tsx
+++ src/pages/camera.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Blog from "../components/blogmaker";
 import {Gallery} from "react-grid-gallery";
 import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 import { images, CustomImage } from "../components/images.ts";
 
 const slides = images.map(({ original, width, height }) => ({
